30e9f62b98b3d82d975e6d80bbe6009035b6565f
Ravi5ingh <ravisingh1203@hotmail.com>
2020-10-20 14:01:28 +0100

    create basic flask API with 1 endpoint

--- app/run.py
+++ app/run.py
@@ -1,1 +1,16 @@
-print('Hello World :D')
+import flask as fl
+
+app = fl.Flask(__name__)
+
+@app.route('/say')
+def say():
+
+    text = fl.request.args.get('text', '')
+
+    return text
+
+def main():
+    app.run(host='0.0.0.0', port=3001, debug=True)
+
+if __name__ == '__main__':
+    main()
