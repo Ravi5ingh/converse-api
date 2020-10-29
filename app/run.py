6f856c71b77b41e2108ff4a4c52a4dbd5b7b355e
Ravi5ingh <ravisingh1203@hotmail.com>
2020-10-29 20:00:29 +0000

    make API work

--- app/run.py
+++ app/run.py
@@ -1,13 +1,20 @@
 import flask as fl
+import utility.util as ut
 
 app = fl.Flask(__name__)
+
+# Load model
+model_filename = __file__[0:__file__.rindex('\\')] + '\\..\\models\\model.pkl'
+model = ut.read_pkl(model_filename)
 
 @app.route('/say')
 def say():
 
     text = fl.request.args.get('text', '')
 
-    return text
+    print(text)
+
+    return model.predict(text)
 
 def main():
     app.run(host='0.0.0.0', port=3001, debug=True)
